3c01dc78397e788297b115ac22712db6b6880c48
note-9 <heemanshbhawsar2004@gmail.com>
2025-07-22 01:36:05 +0530

    initial commit

---
+++ server.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // For ip/op operations
+#include <string> // for using std::string
+#include <stdexcept> // for throwing std exceptions
+#include <sstream> // for string stream operations
+#include <map> // for storing key value pairs
+
+// C - style headers for low level socket programming
+#include <sys/socket.h> 
+#include <netinet/in.h> // for sockaddr_in structure and internet addresses
+#include <unistd.h> // for close function
+#include <thread> // to handle multiple clients
+#include <cstring> // for memset() to clear memory
+
+int main(){
+    
+    return 0;
+}
